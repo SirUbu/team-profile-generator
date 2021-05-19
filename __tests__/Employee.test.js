0fc7f445d0ab417938cf0a153924207a4bcc1499
Sir Ubu <thesirubu@gmail.com>
2021-05-18 18:23:25 -0600

    Added employee class operator.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,41 @@
 // TESTS FOR EMPLOYEE CLASS OPERATOR
+
+// require dependencies
+const Employee = require('../lib/Employee');
+
+test('creates an employee object', () => {
+    const employee = new Employee('John Doe', '341222', 'john.doe@company.com');
+
+    expect(employee.name).toBe('John Doe');
+    expect(employee.name).toEqual(expect.any(String));
+
+    expect(employee.id).toBe(341222);
+    expect(employee.id).toEqual(expect.any(Number));
+
+    expect(employee.email).toBe('john.doe@company.com');
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+test("gets employee's name", () => {
+    const employee = new Employee('John Doe', '341222', 'john.doe@company.com');
+
+    expect(employee.getName()).toEqual('John Doe');
+});
+
+test("gets employee's id", () => {
+    const employee = new Employee('John Doe', '341222', 'john.doe@company.com');
+
+    expect(employee.getId()).toEqual(341222);
+});
+
+test("gets employee's email", () => {
+    const employee = new Employee('John Doe', '341222', 'john.doe@company.com');
+
+    expect(employee.getEmail()).toEqual('john.doe@company.com');
+});
+
+test("gets employee's role", () => {
+    const employee = new Employee('John Doe', '341222', 'john.doe@company.com');
+
+    expect(employee.getRole()).toEqual('Employee');
+});
